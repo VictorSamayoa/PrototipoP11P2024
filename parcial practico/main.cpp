e52576f64fb0efa8901a7a6c2171e443c5d7abf3
Victor Josue Samayoa Ortiz <josuesamayoa064@gmail.com>
2024-02-28 09:54:16 -0600

    primer commit

---
+++ parcial practico/main.cpp
@@ -0,0 +1,52 @@
+/*Victor josue samayoa ortiz
+Carnet: 9959-23-3424
+
+
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#define PUNTOS 50
+#define COLUMNAS 4
+#define EQUIPOS 5
+
+
+void imprimirmatriz(float matriz[EQUIPOS][COLUMNAS+1]);
+
+struct L1{
+string nombre;
+int a2010;
+int a2011;
+int a2012;
+int a2014;
+int Puntos;
+};
+
+
+struct L2{
+string nombre;
+int a2010;
+int a2011;
+int a2012;
+int a2014;
+int Puntos;
+};
+
+
+struct L3{
+string nombre;
+int a2010;
+int a2011;
+int a2012;
+int a2014;
+int Puntos;
+};
+
+
+
